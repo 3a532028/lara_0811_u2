25ba2d4606c3046106c4761fe57748e494de90e2
Bruce <s3a532028@student.ncut.edu.tw>
2018-10-30 22:11:03 +0800

    P4_為Route 命名

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return 'welcome';
-});
+//Route::get('/', function () {
+//    return 'welcome';
+//});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return redirect('welcome');
-});
+//Route::get('/', function () {
+//    return redirect('welcome');
+//});
 
-Route::get('hello/{name?}', function ($name='Everybody') {
+//Route::get('hello/{name?}', function ($name='Everybody') {
+//    return 'Hello, '.$name;
+//});
+
+Route::get('hello/{name?}',['as'=>'hello.index', function ($name='Everybody') {
     return 'Hello, '.$name;
-});
+}]);
